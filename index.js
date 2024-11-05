7fe718343310d9c6f9472b0ae1345a0b9e026de2
Damien Reimert <damien.reimert@gmail.com>
2024-11-05 11:44:33 +0100

    Correction

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const size = argv.size
 // @ts-ignore
 const port = argv.port
 const url = `http://localhost:${port}`
-const id = 'Voir : Prenons un peu de <i>hash</i>'
+const id = getIdFromString(url) // 'Voir : Prenons un peu de <i>hash</i>'
 
 // Initialisation de la configuration du nœud
 const config = {
@@ -119,17 +119,33 @@ app.get('/', (req, res) => {
 app.get('/db/:key', async (req, res) => {
   console.log('GET /db', req.params.key)
 
-  res.json(db[req.params.key])
+  const id = getIdFromString(req.params.key)
+
+  if (idIsInInterval(id)) {
+    res.json(db[req.params.key])
+  } else {
+    const value = await got(config.successor.url + '/db/' + req.params.key).json()
+    res.json(value)
+  }
 })
 
 app.put('/db/:key', async (req, res) => {
   console.log('PUT /db', req.params.key, req.body.value)
 
-  console.log('req', req);
+  const id = getIdFromString(req.params.key)
 
-  db[req.params.key] = req.body.value
+  if (idIsInInterval(id)) {
+    db[req.params.key] = req.body.value
+    res.json(config.url)
+  } else {
+    const url = await got.put({
+      url: `${config.successor.url}/db/${req.params.key}`,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ value: req.body.value })
+    }).json()
 
-  res.json(true)
+    res.json(url)
+  }
 })
 
 app.get('/keys', (req, res) => {
@@ -140,9 +156,13 @@ app.get('/keys', (req, res) => {
 
 app.get('/lookup/:id(\\d+)', async (req, res) => {
   console.log('GET /lookup', req.params.id)
-  
-  // idIsInInterval peut vous être utile ;)
-  res.json('TODO')
+
+  if (idIsInInterval(parseInt(req.params.id))) {
+    res.json(config.url)
+  } else {
+    const url = await got(config.successor.url + '/lookup/' + req.params.id).json()
+    res.json(url)
+  }
 })
 
 app.get('/config/:key', (req, res) => {
@@ -160,20 +180,121 @@ app.get('/config', (req, res) => {
 app.post('/join', async (req, res) => {
   console.log('POST /join', req.body.url)
 
-  // Cf. cli.js pour plus d'exemples
-  // const res = await got.post({
+  // V1 à deux noeuds
+
+  // const add = await got.post({
   //   url: `${req.body.url}/add`,
   //   headers: { 'Content-Type': 'application/json' },
   //   body: JSON.stringify({ url: config.url })
   // })
 
-  res.json('TODO')
+  // config.successor = {
+  //   id: getIdFromString(req.body.url),
+  //   url: req.body.url,
+  // }
+  // config.predecessor = {
+  //   id: getIdFromString(req.body.url),
+  //   url: req.body.url,
+  // }
+
+  // /**
+  //  * @type string[]
+  //  */
+  // const keys = await got(`${req.body.url}/keys`).json()
+
+  // console.info('Syncing keys')
+
+  // for (const key of keys) {
+  //   // TODO : filter only keys that are in the range of the new node
+  //   console.log(key)
+  //   const value = await got(`${req.body.url}/db/${key}`).json()
+  //   console.log(value)
+  //   db[key] = value
+  // }
+
+  // res.json(true)
+
+  // V2 à plusieurs noeuds
+
+  // - Appeler la commande `lookup` du nœud cible pour récupérer le nœud responsable de la valeur du nœud appelant sur l'anneau.
+  // - Appeler la commande `add` du nœud responsable.
+  // - Récupérer le prédécesseur du nœud responsable.
+  // - Appeler la commande `add` du prédécesseur.
+  // - Mettez à jour le successeur et le prédécesseur du nœud appelant.
+  // - Copier les clefs dont le nœud est responsable.
+
+  const lookup = await got(`${req.body.url}/lookup/${config.id}`).json()
+
+  console.log('lookup', lookup)
+
+  const predecessor = await got(`${lookup}/config/predecessor`).json()
+
+  const add = await got.post({
+    url: `${lookup}/add`,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url: config.url })
+  })
+
+  const addPredecessor = await got.post({
+    url: `${predecessor.url}/add`,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url: config.url })
+  })
+
+  config.successor = {
+    id: getIdFromString(lookup),
+    url: lookup,
+  }
+
+  config.predecessor = {
+    id: getIdFromString(predecessor.url),
+    url: predecessor.url,
+  }
+
+  const keys = await got(`${lookup}/keys`).json()
+
+  console.info('Syncing keys')
+
+  console.log('Config:', config);
+
+  res.json(true)
 })
 
 app.post('/add', (req, res) => {
   console.log('POST /add', req.body.url)
 
-  res.json('TODO')
+  // V1 à deux noeuds
+  // config.successor = {
+  //   id: getIdFromString(req.body.url),
+  //   url: req.body.url,
+  // }
+  // config.predecessor = {
+  //   id: getIdFromString(req.body.url),
+  //   url: req.body.url,
+  // }
+
+  // V2 à plusieurs noeuds
+  const id = getIdFromString(req.body.url)
+
+  if (idIsInInterval(id, config.id, config.successor.id - 1)) {
+    console.log('update successor', id, config.id, config.successor.id - 1);
+
+    config.successor = {
+      id,
+      url: req.body.url,
+    }
+  }
+
+  if (idIsInInterval(id, config.predecessor.id, config.id - 1)) {
+    console.log('update predecessor', id, config.predecessor.id, config.id - 1);
+
+    config.predecessor = {
+      id,
+      url: req.body.url,
+    }
+  }
+
+  res.json(true)
 })
 
 // Lancement du serveur
